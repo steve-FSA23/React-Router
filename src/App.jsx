b645f196a9a1d9592dd8ad86c86b5cebf7fedf03
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-02-07 13:53:18 -0500

    completed challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
 
 function App() {
     return (
         <div id="container">
-            <h1>Hello React Router!</h1>
-            <div id="navbar">{/* navigation here */}</div>
+            <h1 className="title">Hello React Router!</h1>
+            <div id="navbar">
+                <Link to="/">Home</Link>
+                <Link to="/blue">Blue</Link>
+                <Link to="/red">Red</Link>
+            </div>
             <div id="main-section">
                 <Routes>
-                    <Route path="/blue" element={<h1>Blue</h1>} />
-                    <Route path="/red" element={<h1>Red</h1>} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blue" element={<Blue />} />
+                    <Route path="/red" element={<Red />} />
                 </Routes>
             </div>
         </div>
---
+++ src/components/Red.jsx
@@ -0,0 +1,9 @@
+const Red = () => {
+    return (
+        <div className="red">
+            <h1>RED</h1>
+        </div>
+    );
+};
+
+export default Red;
