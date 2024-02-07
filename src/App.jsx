ba81f00079b98c408bf1ad9cbfdd685192f71b46
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-02-07 13:23:33 -0500

    initialize git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <div id="container">
+            <h1>Hello React Router!</h1>
+            <div id="navbar">{/* navigation here */}</div>
+            <div id="main-section">
+                <Routes>
+                    <Route path="/blue" element={<h1>Blue</h1>} />
+                    <Route path="/red" element={<h1>Red</h1>} />
+                </Routes>
+            </div>
+        </div>
+    );
+}
+
+export default App;
